f80251abfb267243b2a1331b920fa248588d8ece
Jan van Rouwendal <janvanrouwendal@MacBook-van-Jan.local>
2018-08-20 12:30:20 +0200

    Boilerplate ver 1_0

---
+++ js/quiz.js
@@ -0,0 +1,57 @@
+"use strict"
+
+/**
+ * Add actions to page buttons 
+ */
+function addButtonActions() {
+    var startButton = document.getElementById('button-start');
+    var questionsButton = document.getElementById('button-questions');
+
+    startButton.addEventListener("click", function () {
+        showStartPage();
+    });
+    questionsButton.addEventListener("click", function () {
+        showQuestionsPage();
+    });
+}
+
+/**
+ * Hide all pages
+ */
+function hideAllPages() {
+    var startPage = document.getElementById('page-start');
+    var questionsPage = document.getElementById('page-questions');
+
+    startPage.style.display = 'none';   
+    questionsPage.style.display = 'none';
+}
+
+/**
+ * Show start page
+ */
+function showStartPage() {
+    var page = document.getElementById('page-start');
+    
+    hideAllPages();
+
+    page.style.display = 'block';
+
+    console.info('Je bent nu op de startpagina');
+}
+
+/**
+ * Show questions page
+ */
+function showQuestionsPage() {
+    var page = document.getElementById('page-questions');
+    
+    hideAllPages();
+
+    page.style.display = 'block';
+
+    console.info('Je bent nu op de vragenpagina');
+}
+
+// Initialize
+addButtonActions();
+showStartPage();
