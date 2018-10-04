a5ec3f588524b680e23c9a734b15b9c36e5d8031
Avinash B <S1132653@student.windesheim.nl>
2018-10-04 16:22:51 +0200

    Manier van seconden berekenen veranderen

--- js/quiz.js
+++ js/quiz.js
@@ -145,7 +145,8 @@ var answerButtonClickable, studentInfo, startTime, endTime;
 // Converteert milliseconden naar minuten en seconden, met dank aan https://stackoverflow.com/questions/21294302/converting-milliseconds-to-minutes-and-seconds-with-javascript
 function millisToMinutesAndSeconds(millis) {
     var minutes = Math.floor(millis / 60000);
-    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    // var seconds = ((millis % 60000) / 1000).toFixed(0);
+    var seconds = Math.floor((millis % 60000) / 1000);
     return minutes + " minuten en " + seconds + " seconden";
   }
 
